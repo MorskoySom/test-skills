3678b3c31008942125714068b56100a79d1bd7cd
MorskoySom <Somovvitvit@gmail.com>
2024-09-11 15:17:20 +0300

    add select filter

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,13 @@ import './App.css'
 export class App extends Component {
   state = {
     allQuestions: bodyQuestions,
+    categoryType: "географія",
   }
 
+  changeCategoryType = newTypeCategory => {
+    this.setState({ categoryType: newTypeCategory })
+  };
+  
   getQuestionsByCategory = (category) => {    
     const categoryObject = this.state.allQuestions.find(cat => cat.category === category);    
     return categoryObject ? categoryObject.questions : [];
@@ -17,10 +22,20 @@ export class App extends Component {
 
 
   render() {
-    const geographyQuestions = this.getQuestionsByCategory("література");
-    return <>            
-      <FormTestCard />      
-      <TestCardList quests={ geographyQuestions } />      
-    </>
+    const { categoryType } = this.state;
+    const geographyQuestions = this.getQuestionsByCategory(categoryType);
+    const parts = this.state.allQuestions.map(item => item.category);
+
+    return (
+      <>
+        <div>Header</div>  
+        <FormTestCard
+          categories={parts}
+          selectedCategory={categoryType}
+          onChangeCategory={this.changeCategoryType}
+        />      
+        <TestCardList quests={ geographyQuestions } />      
+      </>
+    )
   }
 }
--- src/Components/FormTestCard/FormTestCard.jsx
+++ src/Components/FormTestCard/FormTestCard.jsx
@@ -1,7 +1,23 @@
-export const FormTestCard = () => {
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+
+export const FormTestCard = ({ categories, selectedCategory, onChangeCategory }) => {
     return (
         <>
+            <select value={selectedCategory} onChange={evt => onChangeCategory(evt.target.value)}>               
+                    {categories.map(part => (
+                        <option key={nanoid()} value={part}>
+                            {part}
+                        </option>
+                    ))}                
+            </select>
             <h1>FormTestCard</h1>            
         </>
     )
 }
+
+FormTestCard.propTypes = {
+    categories: PropTypes.array.isRequired,
+    selectedCategory: PropTypes.string.isRequired,
+    onChangeCategory: PropTypes.func.isRequired,
+};
